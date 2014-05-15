d10c173400d5909bbc0f908f4917b82ced76188d
Shoadi Yokoyama <quantumcars@gmail.com>
2014-05-15 20:33:20 +0900

    Add helm-ghc-errors.el

---
+++ helm-ghc-errors.el
@@ -0,0 +1,28 @@
+;;; helm-ghc-errors.el --- Incremental search GHC errors/wanings by Helm
+;;; -*- lexical-binding: t -*-
+
+;; Copyright (C) 2014~         Syoudai Yokoyama
+
+;; Author: Syoudai Yokoyama <thierry.volpiatto@gmail.com>
+;; URL: http://github.com/emacs-helm/helm
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;; Code.
+
+(require 'helm)
+(require 'ghc)
+
+
+(provide 'helm-ghc-errors)
